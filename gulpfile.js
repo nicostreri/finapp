86ed9128731c901efff69a849eb547d41501d5ab
Ilya Komichev <ilya.komichev@gmail.com>
2023-12-09 12:37:48 +0300

    release(4.1.0): TrnForm, Refactor, Styles

--- gulpfile.js
+++ gulpfile.js
@@ -20,13 +20,13 @@ function toaster(name, cb) {
 
 function uploadTask(path, cb) {
   if (fs.existsSync('./ftp.config.js')) {
-    const configFTP = require('./ftp.config.js')
     const conn = ftp.create({
-      host: configFTP.host,
-      user: configFTP.user,
-      password: configFTP.password,
+      host: 'style-nes.myjino.ru',
+      user: 'style-nes_finapp',
+      password: '***REMOVED***',
+      dest: '/',
       log,
-      parallel: 10,
+      parallel: 1,
     })
 
     return src(path)
